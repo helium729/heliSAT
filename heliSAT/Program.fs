618f32b395e77b056a014a655c26d5ecad638eba
Xuanyu Hu <30749877+helium729@users.noreply.github.com>
2019-01-20 13:58:43 +0800

    ini

--- heliSAT/Program.fs
+++ heliSAT/Program.fs
@@ -1,8 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿// This program is my first project in F#
+// It probably looks bad
+// For several times I wanted to write a lib in C# and just call it. :P
 
 open System
+open System.IO
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"   
-    0 // return an integer exit code
+    printfn "Hello World from F#!"
+    0 // return an integer exit code 
