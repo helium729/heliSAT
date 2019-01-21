f2afee91e9cb3c15712b1bbc8bc9894e673b95bd
Xuanyu Hu <30749877+helium729@users.noreply.github.com>
2019-01-21 10:34:26 +0800

    move to .NET Core 2.1

--- heliSAT/Program.fs
+++ heliSAT/Program.fs
@@ -1,12 +1,8 @@
-﻿// This program is my first project in F#
-// It probably looks bad
-// For several times I wanted to write a lib in C# and just call it. :P
-// Learning Functional programming is fun and also painful
+﻿// Learn more about F# at http://fsharp.org
 
 open System
-open System.IO
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    0 // return an integer exit code 
+    0 // return an integer exit code
