3a7347d4f6b35d9cc102c760e7b833e50cd145bb
Xuanyu Hu <30749877+helium729@users.noreply.github.com>
2019-01-21 14:28:03 +0800

    reader

--- heliSAT/Program.cs
+++ heliSAT/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace heliSAT
 {
     class Program
     {
+        static int v_size, c_size;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -12,6 +15,47 @@ namespace heliSAT
             {
                 Console.WriteLine("Please specify the cnf file");
                 return;
+            }
+            var path = args[0];
+
+
+        }
+
+        List<Dictionary<int, bool?>> readFile(string path)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var splited = line.Split(" ");
+                    if (splited[0] == "c")
+                        continue;
+                    else if (splited[0] == "p")
+                    {
+                        v_size = Convert.ToInt32(splited[2]);
+                        c_size = Convert.ToInt32(splited[3]);
+                        continue;
+                    }
+                    else
+                    {
+                        Dictionary<int, bool?> clause = new Dictionary<int, bool?>();
+
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("CNF file exception");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                return null;
+            }
+            finally
+            {
+                GC.Collect();
             }
         }
     }
