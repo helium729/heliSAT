4775f2605e2377a9d53f4b6b895b27ad0c653bee
Xuanyu Hu <30749877+helium729@users.noreply.github.com>
2019-01-21 11:01:20 +0800

    Add read file

--- heliSAT/Program.fs
+++ heliSAT/Program.fs
@@ -1,6 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System.Threading
+
+// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open System.Collections.Generic
+
+let readFile (path:String) =
+    let reader = new StreamReader(path)
+    let list = new List<String>()
+    list.Add(reader.ReadLine())
+    list
+
+let listToDict (list:List<String>) =
+    
+    0
 
 [<EntryPoint>]
 let main argv =
