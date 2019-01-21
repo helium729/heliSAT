daf0a0aeb4f8770f04a25a27ec05f39d5a50ac1f
Xuanyu Hu <30749877+helium729@users.noreply.github.com>
2019-01-21 16:52:26 +0800

    fell into thinking

--- heliSAT/Program.cs
+++ heliSAT/Program.cs
@@ -22,32 +22,62 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace heliSAT
 {
+
     class Program
     {
         static int v_size, c_size;
+        static bool unsat;
 
         static void Main(string[] args)
         {
+            unsat = false;
             Console.WriteLine("Hello World!");
             if (args.Length == 0)
             {
                 Console.WriteLine("Please specify the cnf file");
                 return;
-            }
+            }            
             var path = args[0];
             var clauses = readFile(path);
-            
+
+            Dictionary<int, bool> result = new Dictionary<int, bool>();
+            var sus = findSuspecious(clauses).Keys.ToArray();
+            var suppose = new bool?[sus.Length];
+            for (int i = 0; i < suppose.Length; i++)
+                suppose[i] = null;
+
+            bool sing = true;
+            int susPointer = 0;
+            Dictionary<int, bool> bk = new Dictionary<int, bool>();
+            while (true)
+            {
+                while (sing)
+                {
+                    sing = singleClause(ref clauses, ref result);
+                    if (unsat && susPointer == 0)//ToDo
+                    {
+                        Console.WriteLine("unsat");
+                        return;
+                    }
+                }
+                if (clauses.Count == 0)
+                    break;
+
+            }
+
+            //return;
         }
 
-        static List<Dictionary<int, bool?>> readFile(string path)
+        static List<Dictionary<int, bool>> readFile(string path)
         {
             try
             {
-                List<Dictionary<int, bool?>> r = new List<Dictionary<int, bool?>>();
+                List<Dictionary<int, bool>> r = new List<Dictionary<int, bool>>();
                 StreamReader reader = new StreamReader(path);
                 while (!reader.EndOfStream)
                 {
@@ -63,13 +93,13 @@ namespace heliSAT
                     }
                     else
                     {
-                        Dictionary<int, bool?> clause = new Dictionary<int, bool?>();
+                        Dictionary<int, bool> clause = new Dictionary<int, bool>();
                         foreach (var a in splited)
                         {
                             if (a == "0")
                                 break;
                             int temp = Convert.ToInt32(a);
-                            bool? v = true;
+                            bool v = true;
                             if (temp < 0)
                             {
                                 v = false;
@@ -95,9 +125,75 @@ namespace heliSAT
             }
         }
 
-        static void singleClause(ref List<Dictionary<int, bool>> clauses)
+        static bool singleClause(ref List<Dictionary<int, bool>> clauses, ref Dictionary<int, bool> result)
         {
+            var singles = from a in clauses
+                          where a.Count == 1
+                          select a;
+            if (singles.Count() == 0)
+                return false;
+            foreach (var a in singles)
+            {
+                var t = a.Single();
+                var re = result.TryAdd(t.Key, t.Value);
+                if (!re)
+                {
+                    bool legacy = false;
+                    result.TryGetValue(t.Key, out legacy);
+                    unsat = legacy != t.Value;
+                    if (unsat)
+                        break;
+                }
+                for (int i = 0; i < clauses.Count; i++)
+                {
+                    bool temp = true;
+                    var b = clauses[i];
+                    var inside = b.TryGetValue(t.Key, out temp);
+                    if (!inside)
+                        continue;
+                    else if (temp == t.Value)
+                    {
+                        clauses.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+                    else
+                    {
+                        clauses[i].Remove(t.Key);
+                    }
+                }
+            }
+            return true;
+        }
 
+        static Dictionary<int, int> findSuspecious(List<Dictionary<int, bool>> clauses)
+        {
+            Dictionary<int, int> suspicious = new Dictionary<int, int>();
+            foreach (var a in clauses)
+            {
+                foreach (var b in a)
+                {
+                    if (suspicious.ContainsKey(b.Key))
+                    {
+                        int temp = 0;
+                        suspicious.TryGetValue(b.Key, out temp);
+                        temp++;
+                        suspicious.Remove(b.Key);
+                        suspicious.TryAdd(b.Key, temp);
+                    }
+                    else
+                        suspicious.TryAdd(b.Key, 1);
+                }
+            }
+            var re = from a in suspicious
+                     orderby a.Value descending
+                     select a;
+            Dictionary<int, int> r = new Dictionary<int, int>();
+            foreach (var a in re)
+            {
+                r.Add(a.Key, a.Value);
+            }
+            return r;
         }
     }
 }
